dfad0ead0252f1b690b1ff0496de40ccd515c0a5
rogoza-sergo13 <rogoza-sergo@mail.ru>
2021-03-13 11:43:47 +0300

    changes

--- task_12(1).py
+++ task_12(1).py
@@ -1,6 +1,16 @@
 class MyOpen:
-    f = open("file.txt", "w")
-    f.write("Hello World11111!")
-    f = open("file.txt", "r")
-    f.close()
+    def __init__(self, file):
+        self.file = file
+
+    def __enter__(self):
+        try:
+            self.file_er = open(self.file)
+        except IOError:
+            self.file_er = open(self.file, "w")
+        self.file_er = open(self.file, "r")
+        return self.file_er
+
+    def __exit__(self, exp_type, exp_value, traceback):
+        self.file_er.close()
+
 
