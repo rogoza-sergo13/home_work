75b4350798c79b663f80763a190c0810c2022605
rogoza-sergo13 <rogoza-sergo@mail.ru>
2021-04-13 20:14:17 +0300

    exception handling

---
+++ task_12(4).py
@@ -0,0 +1,54 @@
+class Car:
+    def __init__(self, marka, model, speed=0):
+        self.__marka = marka
+        self.__model = model
+        self.__speed = speed
+
+    def zoom_in_speed(self):
+        self.__speed += 5
+        return self.__speed
+
+    def decrease_speed(self):
+        try:
+            if self.__speed < 0:
+                raise ValueError
+        except ValueError:
+            print("Your speed is not really, step on the gas")
+            self.__speed = 0
+        else:
+            self.__speed -= 5
+        return self.__speed
+
+    def stop_spedd(self):
+        try:
+            if self.__speed == 0:
+                raise ValueError
+        except ValueError:
+            print("We've stopped, it's time to move")
+        return self.__speed
+
+    def now_speed(self):
+        print(self.__speed)
+
+    def razvorot(self):
+        self.__speed = -self.__speed
+        return self.__speed
+
+
+h1 = Car('bmw', 'x7')
+
+h1.zoom_in_speed()
+h1.now_speed()
+h1.zoom_in_speed()
+h1.now_speed()
+h1.decrease_speed()
+h1.now_speed()
+h1.razvorot()
+h1.now_speed()
+h1.decrease_speed()
+h1.now_speed()
+h1.zoom_in_speed()
+h1.now_speed()
+h1.decrease_speed()
+h1.now_speed()
+h1.stop_spedd()
